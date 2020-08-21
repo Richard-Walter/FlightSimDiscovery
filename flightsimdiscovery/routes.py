554fbb6c6b968622a02b9195fe71ddd9c2b53ba2
Richard-Walter <willthisnamedo@gmail.com>
2020-08-21 10:43:02 +1000

    add account update fn

--- flightsimdiscovery/routes.py
+++ flightsimdiscovery/routes.py
@@ -1,6 +1,9 @@
+import os
+import secrets
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flightsimdiscovery import app, db, bcrypt
-from flightsimdiscovery.forms import RegistrationForm, LoginForm
+from flightsimdiscovery.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flightsimdiscovery.models import User, Pois
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -61,8 +64,42 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    # randomize the users profile picture so it doesn't conflict with another user
+    random_hex = secrets.token_hex(8)
+    # f_name, f_ext = os.path.splitext(form_picture.filename) # This returns file name and file extension.  we just want extension
+    _, f_ext = os.path.splitext(form_picture.filename) # This returns file name and file extension.  we just want extension
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/img/profile_pics', picture_fn)
+
+    # form_picture.save(picture_path)
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
 @login_required
-@app.route("/account")
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file  #image file is what we defined in our models.py
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+
+        flash('Your account has been updated!', 'success')
+        # want to redirect instead of render template so a get request is called instead of  post saying form resubmissions
+        return redirect(url_for('account')) 
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    image_file = url_for('static', filename='img/profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account',
+                           image_file=image_file, form=form)
