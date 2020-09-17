46f6ae3e582d7d357ca9d2e2b514c2692adbd24d
Richard-Walter <willthisnamedo@gmail.com>
2020-09-17 12:01:28 +1000

    start build xml file

--- flightsimdiscovery/static/js/flightplan.js
+++ flightsimdiscovery/static/js/flightplan.js
@@ -1,18 +1,10 @@
 var flightPath_data = []
 
-function saveFlightPlan(document) {
+function saveFlightPlan(document, flightPath_data) {
 
-    content = 'This is some content';
-    filename = 'flight_plan.fpl';
-
-    var doc = document.implementation.createDocument ('http://www.w3.org/1999/xhtml', 'html', null);
-    var body = document.createElementNS('http://www.w3.org/1999/xhtml', 'body');
-    body.setAttribute('id', 'abc');
-    doc.documentElement.appendChild(body);
-    content = doc
-
-    
-            
+    filename = 'flight_plan.txt';
+    content = buildFlightPlan(flightPath_data);
+   
     const a = document.createElement('a');
     const file = new Blob([content], {type: 'text/plain'});
     
@@ -21,13 +13,47 @@ function saveFlightPlan(document) {
     a.click();
     
     URL.revokeObjectURL(a.href);
-    alert("saving flight plan")
 }
 
 
+function buildFlightPlan(flightPath_data) {
 
-function buildFlightPlan(document) {
+    var content = '';
+    const title_txt = 'Custom departure to Custom Arrival';
+    const crusing_altitude = '7500.000';
+    const departure_id = 'CUSTD';
+    const departure_lla = `S35° 18' 35.73",E146° 48' 40.22",+001500.00`;
+    const destination_id = 'CUSTA';
+    const destination_lla = `S35° 27' 43.54",E147° 59' 49.78",+001640.42`;
 
+    const header =          '<?xml version="1.0" encoding="UTF-8"?>\n' +
+                            '\n'+
+                            '<SimBase.Document Type="AceXML" version="1,0"></SimBase.Document>\n'+
+                            '    <Descr>AceXML Document</Descr>\n'+
+                            '    <FlightPlan.FlightPlan>\n';
+    const title =           `        <Title>${title_txt}</Title>` + '\n';
+    const FPTType =         `        <FPType>VFR</FPType>` + '\n';
+    const CruisingAlt =     `        <CruisingAlt>${crusing_altitude}</CruisingAlt>` + '\n';
+    const DepartureID =     `        <DepartureID>${departure_id}</DepartureID>` + '\n';
+    const DepartureLLA =    `        <DepartureLLA>${departure_lla}</DepartureLLA>` + '\n';
+    const DestinationID =   `        <DestinationID>${destination_id}</DestinationID>` + '\n';
+    const DestinationLLA =  `        <DestinationLLA>${destination_lla}</DestinationLLA>` + '\n';
+
+
+    content += header + title + FPTType + CruisingAlt + DepartureID + DepartureID + DepartureLLA + DestinationID + DestinationLLA;
+
+    console.log(content);
+
+
+    // if (Array.isArray(flightPath_data) && flightPath_data.length>1) {
+    
+    //     for (var i = 0; i < flightPath_data.length; i++) {
+    //         content += flightPath_data[i]['waypoint'] + " --> ";
+    //       }
+    //     content = content.substring(0, content.length-5);  //remove the -->
+    // }
+
+    return content;
 }
 
 function buildFlightPlanModalBody(flightPath_data) {
