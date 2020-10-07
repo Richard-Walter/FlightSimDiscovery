5a530ab7f50c9c2a440856f1c53ec935030ea9c2
Richard Walter <willthisnamedo@gmail.com>
2020-10-08 10:25:24 +1100

    move database out of repo

--- flightsimdiscovery/config.py
+++ flightsimdiscovery/config.py
@@ -1,5 +1,14 @@
 import os
 import json
+from pathlib import Path
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+config_dir = Path(basedir).parents[1]
+support_dir = os.path.join(config_dir, 'FSDSupport')
+database_URI = 'sqlite:///' + str(support_dir) + '//fsdiscovery.db'
+print('Base Directory', basedir)
+print('Config  Directory', config_dir)
+print('Database URI', database_URI)
 
 # used in production
 with open('/etc/config.json') as config_file:
@@ -7,7 +16,8 @@ with open('/etc/config.json') as config_file:
 
 class Config:
     SECRET_KEY = config.get('FSDISCOVERY_SK')
-    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
+    # SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = database_URI
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
@@ -15,9 +25,12 @@ class Config:
     MAIL_PASSWORD = config.get('EMAIL_PASS')
     GM_KEY = config.get('GM_KEY')
 
+
+
 # class Config:
 #     SECRET_KEY = os.environ.get('FSDISCOVERY_SK')
-#     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+#     # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+#     SQLALCHEMY_DATABASE_URI = database_URI
 #     MAIL_SERVER = 'smtp.googlemail.com'
 #     MAIL_PORT = 587
 #     MAIL_USE_TLS = True
