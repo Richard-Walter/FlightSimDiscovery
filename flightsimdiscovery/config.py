af740973586e2afe6c5c93c9c5a893817e971c5d
Richard Walter <willthisnamedo@gmail.com>
2020-10-08 11:54:56 +1100

    production config

--- flightsimdiscovery/config.py
+++ flightsimdiscovery/config.py
@@ -11,30 +11,30 @@ print('Config  Directory', config_dir)
 print('Database URI', database_URI)
 
 # used in production
-# with open('/etc/config.json') as config_file:
-#     config = json.load(config_file)
-
-# class Config:
-#     SECRET_KEY = config.get('FSDISCOVERY_SK')
-#     # SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
-#     SQLALCHEMY_DATABASE_URI = database_URI
-#     MAIL_SERVER = 'smtp.googlemail.com'
-#     MAIL_PORT = 587
-#     MAIL_USE_TLS = True
-#     MAIL_USERNAME = config.get('EMAIL_USER')
-#     MAIL_PASSWORD = config.get('EMAIL_PASS')
-#     GM_KEY = config.get('GM_KEY')
-
-
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
 
 class Config:
-    SECRET_KEY = os.environ.get('FSDISCOVERY_SK')
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = config.get('FSDISCOVERY_SK')
+    # SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_DATABASE_URI = database_URI
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
-    GM_KEY = os.environ.get('GM_KEY')
+    MAIL_USERNAME = config.get('EMAIL_USER')
+    MAIL_PASSWORD = config.get('EMAIL_PASS')
+    GM_KEY = config.get('GM_KEY')
+
+
+
+# class Config:
+#     SECRET_KEY = os.environ.get('FSDISCOVERY_SK')
+#     # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+#     SQLALCHEMY_DATABASE_URI = database_URI
+#     MAIL_SERVER = 'smtp.googlemail.com'
+#     MAIL_PORT = 587
+#     MAIL_USE_TLS = True
+#     MAIL_USERNAME = os.environ.get('EMAIL_USER')
+#     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+#     GM_KEY = os.environ.get('GM_KEY')
     
