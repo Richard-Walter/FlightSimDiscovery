f5f65018ca29456809d4f914b5e9656968c1e57d
Richard Walter <willthisnamedo@gmail.com>
2020-11-22 13:13:42 +1100

    added share flight plan to export flight plan

--- flightsimdiscovery/static/js/flightplan.js
+++ flightsimdiscovery/static/js/flightplan.js
@@ -21,6 +21,8 @@ function exportFlightPlan(flightPath_data) {
 
   var fp_pois = [];
   flightplan_filename = $("#export-filename-input").val();
+  share_flightplan = $('#export-share_fp').is(":checked");
+  console.log(share_flightplan);
 
   // filename = "Flight Sim Discovery " + yyyymmdd() + ".pln";
   cruise_altitude = $("#cruise_altitude_value").val();
@@ -296,6 +298,11 @@ function buildFlightPlanModalBody(flightPath_data) {
       body_html += "<div class='export-filename form-group mt-3'>";
       body_html += "<label class='mr-2' >Flight Plan Name: </label>";
       body_html += '<input type="text" class="form-control" id="export-filename-input" value="' + flightplan_filename + '">';
+      body_html += "</div>";
+      body_html += "<div class='form-check mt-2'>";
+      body_html += '<input type="checkbox" class="form-check-input" id="export-share_fp">';
+      body_html += "<label class='mr-2' >Share flight plan with the community </label>";
+
       body_html += "</div>";
       body_html += "</form>";
       body_html += 
