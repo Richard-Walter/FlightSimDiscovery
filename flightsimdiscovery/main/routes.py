fbe836a915b0bfabb324e09c35898f09f015faff
Richard Walter <willthisnamedo@gmail.com>
2020-12-15 11:28:47 +1100

    improve look & feel POI flight plan

--- flightsimdiscovery/main/routes.py
+++ flightsimdiscovery/main/routes.py
@@ -18,6 +18,7 @@ main = Blueprint('main', __name__)
 # TODO add tours to each POI - DONE
 # TODO anon user can flag a poi - DONE
 
+# TODO Bug - flightplans and only showing user pois
 # TODO Update FAQ to include flight plans
 # TODO Justify POI flight plan text 
 # TODO get details (most popular) on flight plans and pois visisted by anonymous users
