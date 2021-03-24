d3205ab5632fcfe6a3ff9d31faf00f55630f7cee
Richard-Walter <willthisnamedo@gmail.com>
2021-03-25 10:31:20 +1100

    updated TODOs

--- flightsimdiscovery/main/routes.py
+++ flightsimdiscovery/main/routes.py
@@ -16,10 +16,11 @@ from flightsimdiscovery.SimFlights.Flights import Flights
 
 main = Blueprint('main', __name__)
 
-# TODO Bug in MSFS that doesn't display flightpath to waypoints if ATCWaypoint ID > 6 chars for G3X & G1000
+# DONE Bug in MSFS that doesn't display flightpath to waypoints if ATCWaypoint ID > 6 chars for G3X & G1000
+# TODO Share flightplans not showing up in Saved Flight Plans
 # TODO Unchecking show my flights reloads page and shows as ticked
+# TODO Add section on most popular flights
 # TODO add flights from VOlanta - in progress
-# TODO do flight path processing on front end in Javascript
 # TODO allow users to upload photo of location
 
 @main.route('/robots.txt')
