e49151e803e016c4a56eeb3fad2c4b357e7e14f2
Richard-Walter <willthisnamedo@gmail.com>
2021-06-06 08:35:54 +1000

    poi IW closes when clicking outside window

--- flightsimdiscovery/main/routes.py
+++ flightsimdiscovery/main/routes.py
@@ -15,7 +15,9 @@ from flightsimdiscovery.flightplans.utils import get_user_flightplans
 
 main = Blueprint('main', __name__)
 
-# DONE Bug in MSFS that doesn't display flightpath to waypoints if ATCWaypoint ID > 6 chars for G3X & G1000
+# NOTE Bug in MSFS that doesn't display flightpath to waypoints if ATCWaypoint ID > 6 chars for G3X & G1000
+
+
 # DONE Fix bug where Clicking on Map ICon ina top ten page gives 404
 # DONE add photogrammetry as a category
 # DONE add account setting that bypasses banner and goes straight to map
